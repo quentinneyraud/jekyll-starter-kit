d4c31e827c3b8d0c51bc52806f30d5795c90422c
Quentin Neyraud <quentin.neyraud@gmail.com>
2017-08-04 16:38:57 +0200

    update gulp to v4.0 to fix watch issue
    Former-commit-id: aa5b44c1d99785953d0855fbc5a4acc7dd6a7931 [formerly 9c4f7c42d9dd4eb80f035dcc833a6d8bd23b2af8]
    Former-commit-id: 7bbbe2274db3dfa983a28b29aa82f38269bb6179

--- build/gulp-tasks/build-svg.js
+++ build/gulp-tasks/build-svg.js
@@ -4,49 +4,55 @@ import svgstore from 'gulp-svgstore'
 import path from 'path'
 import del from 'del'
 import rename from 'gulp-rename'
+import debug from 'debug'
 import config from '../../config/index'
 
+const dbg = debug('app:svgmin')
 const paths = config.utils_paths
 
-gulp.task('clean-svg', () => {
+const cleanSvg = () => {
+  dbg('🗑  Cleaning sprite.')
   return del(path.resolve(config.svg_sprite_path, config.svg_sprite_name), {
     force: true
   })
-})
+}
 
-gulp.task('build-svg', ['clean-svg'], () => {
+const buildSvg = () => {
   return gulp.src(paths.client(config.svg_paths))
+    .on('end', () => {
+      dbg(`🔨  ${config.svg_sprite_name} created.`)
+    })
     .pipe(svgmin((file) => {
       const prefix = path.basename(file.relative, path.extname(file.relative));
       return {
         plugins: [{
-            cleanupIDs: {
-              prefix: prefix + '-',
-              minify: true
-            }
-          }, {
-            removeTitle: true
-          }, {
-            removeViewBox: false
-          }, {
-            removeUselessStrokeAndFill: true
-          }, {
-            removeAttrs:{
-              attrs: [
-                'fill',
-                'fill-rule',
-                'clip-rule',
-                'sketch:type',
-                'sketch:type',
-                'xmlns:sketch',
-                'xmlns:xlink'
-              ]
-            }
+          cleanupIDs: {
+            prefix: prefix + '-',
+            minify: true
           }
+        }, {
+          removeTitle: true
+        }, {
+          removeViewBox: false
+        }, {
+          removeUselessStrokeAndFill: true
+        }, {
+          removeAttrs: {
+            attrs: [
+              'fill',
+              'fill-rule',
+              'clip-rule',
+              'sketch:type',
+              'sketch:type',
+              'xmlns:sketch',
+              'xmlns:xlink'
+            ]
+          }
+        }
         ]
       }
     }))
-    .pipe(rename(function (path) {
+    .pipe(rename((path) => {
       path.basename = 'icon-' + path.basename;
     }))
     .pipe(svgstore({
@@ -57,9 +63,15 @@ gulp.task('build-svg', ['clean-svg'], () => {
     }))
     .pipe(rename(config.svg_sprite_name))
     .pipe(gulp.dest(paths.dist(config.svg_sprite_path)))
-})
+}
 
-gulp.task('watch-svg', () => {
-  console.log('test', paths.client(config.svg_paths))
-  gulp.watch(paths.client(config.svg_paths), ['build-svg'])
-})
+const watchSvg = () => {
+  dbg(`🕶  watching svg source folder : ${paths.client(config.svg_paths)}`)
+  gulp.watch(paths.client(config.svg_paths), buildSvgTask)
+}
+
+const watchSvgTask = watchSvg
+const buildSvgTask = gulp.series(cleanSvg, buildSvg)
+
+gulp.task('build-svg', buildSvgTask)
+gulp.task('watch-svg', watchSvgTask)
