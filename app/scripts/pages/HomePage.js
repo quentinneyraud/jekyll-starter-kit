4c31323254819471b7a14edc4bfac2c237533028
Quentin Neyraud <quentin.neyraud@gmail.com>
2017-08-04 18:06:06 +0200

    add globals demo
    Former-commit-id: c393440b3adba96a635fb8323b5073170680590c

--- app/scripts/pages/HomePage.js
+++ app/scripts/pages/HomePage.js
@@ -20,6 +20,11 @@ export default class HomePage extends Page {
   onEnter () {
     super.onEnter()
 
+    // globals demo
+    const apiEndpoint = (PRODUCTION) ? 'http://api-prod/' : 'http://api-dev/'
+    console.log(apiEndpoint)
+
+    // addInterval demo
     const intervalId = window.setInterval(() => {
       console.log('I will be killed on page leave 😬')
     }, 1000)
