67ac825319f5eefb809576216d6e6d417f4e1bf4
Quentin Neyraud <quentin.neyraud@gmail.com>
2017-08-04 16:29:26 +0200

    Update packages
    Former-commit-id: dfdfa392f45457464c69812a706c148e4762665c

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+var cssnano = require('cssnano')
+
+module.exports = {
+  plugins: [
+    cssnano({
+      autoprefixer: {
+        add: true,
+        remove: true,
+        browsers: ['last 2 versions']
+      },
+      discardComments: {
+        removeAll: true
+      },
+      discardUnused: false,
+      mergeIdents: false,
+      reduceIdents: false,
+      safe: true,
+      sourcemap: true
+    })
+  ]
+}
