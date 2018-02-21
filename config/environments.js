e94d38e7aaeb910136b449e75f0fcc08c3136df4
Quentin Neyraud <quentin.neyraud@gmail.com>
2018-02-21 21:00:40 +0200

    remove config & build files related to webpack

--- config/environments.js
+++ config/environments.js
@@ -1,12 +1,2 @@
 export default {
-  development: (config) => ({
-    publicPath: `http://${config.address}:${config.port}/`
-  }),
-
-  production: (config) => ({
-    devtool: false,
-    assetsNameJs: 'assets/js/[name].[hash].js',
-    assetsNameImg: 'assets/img/[name].[hash].[ext]',
-    assetsNameCss: 'assets/css/style.[hash].css'
-  })
 }
