5c7f4fe254b9e7c6a64c28aeed799e3828d5ed03
Unknown <quentin.neyraud@gmail.com>
2018-02-22 12:29:28 +0200

    Reload page on build

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,8 @@ import App from './App'
 /* eslint no-new:0 */
 new App()
 
-if (module.hot) {
-  module.hot.accept()
+if (module.hot.accept) {
+  module.hot.accept(() => {
+    location.reload(true)
+  })
 }
